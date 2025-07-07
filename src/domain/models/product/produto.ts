04ae8bbdbcaad3b3bc51de9b2664d512f2526590
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-07 20:57:13 -0300

    feat: ensure create model for product

---
+++ src/domain/models/product/produto.ts
@@ -0,0 +1,14 @@
+export interface ProductModel {
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+}
+
+export interface ProductModel {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+}
