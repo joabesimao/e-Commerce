6872907d016e447c0e6dab0e3eb1785a47a251e2
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-09 16:25:56 -0300

    feat: ensure create contact model

---
+++ src/domain/models/contacts/contact.ts
@@ -0,0 +1,12 @@
+export interface ContactsModel {
+  phone: number;
+  phoneSecundary: number;
+  email: string;
+}
+
+export interface Contacts {
+  id: number;
+  phone: number;
+  phoneSecundary: number;
+  email: string;
+}
