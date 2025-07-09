ff6b860583739b48e94a021da6f1eaf5b7c3da0b
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-09 16:06:13 -0300

    feat: ensure create address model

---
+++ src/domain/models/address/address.ts
@@ -0,0 +1,16 @@
+export interface AddressModel {
+  street: string;
+  neighborhood: string;
+  numberHouse: number;
+  reference: string;
+  city: string;
+}
+
+export interface Address {
+  id: number;
+  street: string;
+  neighborhood: string;
+  numberHouse: number;
+  reference: string;
+  city: string;
+}
