77ec342e90f10c62e4457f08fef2f65bba08ff55
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-11 20:30:58 -0300

    test: ensure load one a clients on success

---
+++ src/data/usecases/client-usescases/load-one-client/db-load-one-client.spec.ts
@@ -0,0 +1,73 @@
+import { DbLoadOneClient } from "./db-load-one-client";
+import { Client } from "../../../../domain/models/client/client";
+import { LoadOneClientRepository } from "../../../protocols/db/client/load-one-client";
+
+interface SutTypes {
+  sut: DbLoadOneClient;
+  loadOneClientRepositoryStub: LoadOneClientRepository;
+}
+
+const makeClient = (): Client => ({
+  id: 1,
+  name: "any_name",
+  cpf: "any_cpf",
+  address: {
+    cep: "any_cep",
+    city: "any_city",
+    neighborhood: "any_neighborhood",
+    numberHouse: 1,
+    reference: "any_ref",
+    street: "any_street",
+  },
+  contact: {
+    email: "any_email",
+    phone: 55555555,
+    phoneSecundary: 6666666,
+  },
+});
+
+const makeLoadOneClientRepository = (): LoadOneClientRepository => {
+  class LoadOneClientRepositoryStub implements LoadOneClientRepository {
+    async loadOne(id: number): Promise<Client> {
+      return new Promise((resolve) => resolve(makeClient()));
+    }
+  }
+  return new LoadOneClientRepositoryStub();
+};
+
+const makeSut = (): SutTypes => {
+  const loadOneClientRepositoryStub = makeLoadOneClientRepository();
+  const sut = new DbLoadOneClient(loadOneClientRepositoryStub);
+  return {
+    sut,
+    loadOneClientRepositoryStub,
+  };
+};
+
+describe("DbLoadOneClient Usecase", () => {
+  const id = 8;
+
+  test("Should call LoadOneClientRepository with correct values", async () => {
+    const { sut, loadOneClientRepositoryStub } = makeSut();
+    const addSpy = jest.spyOn(loadOneClientRepositoryStub, "loadOne");
+    await sut.loadOne(id);
+    expect(addSpy).toHaveBeenCalledWith(8);
+  });
+
+  test("Should load one client on success", async () => {
+    const { sut } = makeSut();
+    const client = await sut.loadOne(id);
+    expect(client).toEqual(makeClient());
+  });
+
+  test("Should throw if LoadOneClientRepository throws", async () => {
+    const { sut, loadOneClientRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadOneClientRepositoryStub, "loadOne")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error("")))
+      );
+    const promise = sut.loadOne(id);
+    await expect(promise).rejects.toThrow();
+  });
+});
