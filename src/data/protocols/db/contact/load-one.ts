b215a8fc35ec5689380d9611bdb33e6d547d6cf4
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-13 20:27:14 -0300

    feat: ensure load one contact

--- src/data/protocols/db/contact/load-one.ts
+++ src/data/protocols/db/contact/load-one.ts
@@ -2,5 +2,5 @@ import { Address } from "../../../../domain/models/address/address";
 import { Contacts } from "../../../../domain/models/contacts/contact";
 
 export interface LoadOneContactRepository {
-  loadOne(): Promise<Contacts>;
+  loadOne(id: number): Promise<Contacts>;
 }
