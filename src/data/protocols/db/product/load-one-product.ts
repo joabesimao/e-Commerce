aee8d3374f87bc27ff1e2908571a63d79ce996e3
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-15 15:14:10 -0300

    feat: ensure load one product

--- src/data/protocols/db/product/load-one-product.ts
+++ src/data/protocols/db/product/load-one-product.ts
@@ -1,5 +1,5 @@
 import { Product } from "../../../../domain/models/product/product";
 
 export interface LoadOneProductRepository {
-  loadOne(): Promise<Product>;
+  loadOne(id: number): Promise<Product>;
 }
