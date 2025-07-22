c88d19fe327afc6e1d97e50a0385a11ef3be9d63
Joabe Simão <simaojoabecosta@gmail.com>
2025-07-22 19:39:35 -0300

    feat: ensure create client repository

---
+++ src/infra/db/mysql/client-repository/client.ts
@@ -0,0 +1,64 @@
+import { AddClientRepository } from "../../../../data/protocols/db/client/add-client";
+import { DeleteClientRepository } from "../../../../data/protocols/db/client/delete-client";
+import { LoadAllClientRepository } from "../../../../data/protocols/db/client/load-all-client";
+import { LoadOneClientRepository } from "../../../../data/protocols/db/client/load-one-client";
+import { UpdateClientRepository } from "../../../../data/protocols/db/client/update-client";
+import { Client } from "../../../../domain/models/client/client";
+import { AddClientModel } from "../../../../domain/usescase/client/add-client/add-client";
+import { prisma } from "../helper/index";
+
+export class ClientRepository
+  implements
+    AddClientRepository,
+    LoadAllClientRepository,
+    LoadOneClientRepository,
+    UpdateClientRepository,
+    DeleteClientRepository
+{
+  async add(client: AddClientModel): Promise<Client> {
+    const { name, address, contact, cpf } = client;
+    const addClient = await prisma.client.create({
+      data: { name: name, address: address, contact: contact, cpf: cpf },
+    });
+    return addClient as unknown as Client;
+  }
+
+  async loadAll(): Promise<Client[]> {
+    const loadAllClient = await prisma.client.findMany({
+      select: {
+        id: true,
+        name: true,
+        address: true,
+        contact: true,
+        cpf: true,
+      },
+    });
+    return loadAllClient;
+  }
+
+  async loadOne(id: number): Promise<Client> {
+    const oneClient = await prisma.client.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    return oneClient;
+  }
+  async update(id: number, info: Partial<Client>): Promise<Client> {
+    const updateClient = await prisma.client.update({
+      where: {
+        id: Number(id),
+      },
+      data: { ...info },
+    });
+    return updateClient;
+  }
+  async delete(id: number): Promise<string> {
+    await prisma.client.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    return "Deletado com Sucesso!";
+  }
+}
