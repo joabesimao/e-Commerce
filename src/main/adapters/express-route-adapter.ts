f0f037b8c1768508e3e3904bb1f5ff8d03a99319
joabesimao <simaojoabecosta@gmail.com>
2025-12-06 11:23:10 -0300

    feat: ensure create adapterRoute

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,21 @@
+import { Controller } from "../../presentation/protocols/controller/controller";
+import { Request, Response } from "express";
+import { HttpRequest } from "../../presentation/protocols/http/http";
+
+export const adapterRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      params: req.params,
+      headers: req.query,
+    };
+    const httpResponse = await controller.handle(httpRequest);
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message,
+      });
+    }
+  };
+};
